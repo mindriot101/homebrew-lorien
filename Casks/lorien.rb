e8e3ce05641d85d398100f4d9fc1fa0b8f591cca
Simon Walker <s.r.walker101@googlemail.com>
2022-06-11 22:52:15 +0100

    Include lorien cask

---
+++ Casks/lorien.rb
@@ -0,0 +1,11 @@
+cask "lorien" do
+  version "0.4.0"
+  sha256 "444da36d35d3c4f767903dcac88333ca442c9a8d5bd6b19fbf9d7536f95ddb44"
+
+  url "https://github.com/mbrlabs/Lorien/releases/download/v#{version}/Lorien_v#{version}_macOS.dmg"
+  name "Lorien"
+  desc "Infinite canvas drawing/whiteboarding app for Windows, Linux and macOS. Made with Godot"
+  homepage "https://github.com/mbrlabs/Lorien"
+
+  app "Lorien.app"
+end
